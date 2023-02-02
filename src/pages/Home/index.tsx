5f0af9680da28559edfcf7d5db456f4f9a87e457
Fernando Cavalcanti <comododk@gmail.com>
2023-02-01 23:42:15 -0300

    fix: timer  count

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
-  startDate?: Date;
+  startDate: Date;
 }
 
 export function Home() {
@@ -41,15 +41,17 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(
-        () =>
-          setAmountSecondsPassed(
-            differenceInSeconds(new Date(), activeCycle.startDate),
-          ),
-        1000,
-      );
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        );
+      }, 1000);
     }
+
+    return () => clearInterval(interval);
   }, [activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -64,6 +66,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
 
     reset();
   }
@@ -76,6 +79,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `IgniteTime - ${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const task = watch('task');
   const isSubmitDisabled = !task;
