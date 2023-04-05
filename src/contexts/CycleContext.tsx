7bd3e36d238096af2308719cc3acc0e0deee7297
Fernando Cavalcanti <comododk@gmail.com>
2023-04-04 23:56:43 -0300

    fix: resetting the stopwatch

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -47,8 +47,6 @@ export function CyclesContextProvider({ children }: CycleContentProviderProps) {
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
     setAmountSecondsPassed(0);
-
-    // reset();
   }
 
   function interruptCurrentCycle() {
