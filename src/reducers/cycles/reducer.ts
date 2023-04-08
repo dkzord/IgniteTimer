1876ab4eef84f07915954c29995c7641f777a99d
Fernando Cavalcanti <comododk@gmail.com>
2023-04-08 13:56:45 -0300

    feat: using Immer to work with immutable states

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,46 +1,41 @@
 import { ActionsTypes } from './actions';
 import { CycleState } from '../../contexts/typings';
 
+import { produce } from 'immer';
+
 export function CyclesReducer(state: CycleState, action: any) {
   switch (action.type) {
     case ActionsTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      };
+      return produce(state, (draf) => {
+        draf.cycles.push(action.payload.newCycle);
+        draf.activeCycleId = action.payload.newCycle.id;
+      });
 
-    case ActionsTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return {
-              ...cycle,
-              interruptedAt: new Date(),
-            };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null,
-      };
+    case ActionsTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
 
-    case ActionsTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return {
-              ...cycle,
-              finishedAt: new Date(),
-            };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null,
-      };
+      if (currentCycleIndex < 0) return state;
+
+      return produce(state, (draf) => {
+        draf.activeCycleId = null;
+        draf.cycles[currentCycleIndex].interruptedAt = new Date();
+      });
+    }
+
+    case ActionsTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) return state;
+
+      return produce(state, (draf) => {
+        draf.activeCycleId = null;
+        draf.cycles[currentCycleIndex].finishedAt = new Date();
+      });
+    }
 
     default:
       return state;
